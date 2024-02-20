79c769e6db6ce9243e10a402bd5c65485eb18500
Luan Lima <lordellu@hotmail.com>
2024-02-19 21:34:35 -0300

    criando servico para disparo de email e refatorando servico de adocao

--- src/main/java/br/com/alura/adopet/api/service/AdocaoService.java
+++ src/main/java/br/com/alura/adopet/api/service/AdocaoService.java
@@ -5,8 +5,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.adopet.api.exception.ValidacaoException;
@@ -21,7 +19,7 @@ public class AdocaoService {
 	private AdocaoRepository repository;
 
 	@Autowired
-	private JavaMailSender emailSender;
+	private EmailService emailService;
 
 	public void solicitar(Adocao adocao) {
 		if (adocao.getPet().getAdotado() == true) {
@@ -51,47 +49,33 @@ public class AdocaoService {
 		adocao.setData(LocalDateTime.now());
 		adocao.setStatus(StatusAdocao.AGUARDANDO_AVALIACAO);
 		repository.save(adocao);
-
-		SimpleMailMessage email = new SimpleMailMessage();
-		email.setFrom("adopet@email.com.br");
-		email.setTo(adocao.getPet().getAbrigo().getEmail());
-		email.setSubject("Solicitação de adoção");
-		email.setText("Olá " + adocao.getPet().getAbrigo().getNome()
-				+ "!\n\nUma solicitação de adoção foi registrada hoje para o pet: " + adocao.getPet().getNome()
-				+ ". \nFavor avaliar para aprovação ou reprovação.");
-		emailSender.send(email);
+		
+		String emailBody = "Olá " + adocao.getPet().getAbrigo().getNome()
+				+ "Uma solicitação de adoção foi registrada hoje para o pet: " + adocao.getPet().getNome()
+				+ ". \nFavor avaliar para aprovação ou reprovação.";
+		emailService.enviarEmail(adocao.getTutor().getEmail(), "Adoção solicitada", emailBody);
 	}
 
 	public void aprovar(Adocao adocao) {
 		adocao.setStatus(StatusAdocao.APROVADO);
 		repository.save(adocao);
 
-		SimpleMailMessage email = new SimpleMailMessage();
-		email.setFrom("adopet@email.com.br");
-		email.setTo(adocao.getTutor().getEmail());
-		email.setSubject("Adoção aprovada");
-		email.setText("Parabéns " + adocao.getTutor().getNome() + "!\n\nSua adoção do pet " + adocao.getPet().getNome()
+		String emailBody = "Parabéns " + adocao.getTutor().getNome() + "Sua adoção do pet " + adocao.getPet().getNome()
 				+ ", solicitada em " + adocao.getData().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"))
 				+ ", foi aprovada.\nFavor entrar em contato com o abrigo " + adocao.getPet().getAbrigo().getNome()
-				+ " para agendar a busca do seu pet.");
-		emailSender.send(email);
-
+				+ " para agendar a busca do seu pet.";
+		emailService.enviarEmail(adocao.getTutor().getEmail(), "Adoção aprovada", emailBody);
 	}
 
 	public void reprovar(Adocao adocao) {
 		adocao.setStatus(StatusAdocao.REPROVADO);
 		repository.save(adocao);
 
-		SimpleMailMessage email = new SimpleMailMessage();
-		email.setFrom("adopet@email.com.br");
-		email.setTo(adocao.getTutor().getEmail());
-		email.setSubject("Adoção reprovada");
-		email.setText("Olá " + adocao.getTutor().getNome() + "!\n\nInfelizmente sua adoção do pet "
+		String emailBody = "Olá " + adocao.getTutor().getNome() + "Infelizmente sua adoção do pet "
 				+ adocao.getPet().getNome() + ", solicitada em "
 				+ adocao.getData().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"))
 				+ ", foi reprovada pelo abrigo " + adocao.getPet().getAbrigo().getNome()
-				+ " com a seguinte justificativa: " + adocao.getJustificativaStatus());
-		emailSender.send(email);
-
+				+ " com a seguinte justificativa: " + adocao.getJustificativaStatus();
+		emailService.enviarEmail(adocao.getTutor().getEmail(), "Adoção reprovada", emailBody);
 	}
 }
